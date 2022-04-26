6c007c1928b1b0a96c5254132a7810c053cb8491
Maksym Pshenychnyi <maksim_pshenichniy@mail.ru>
2022-04-26 20:21:01 +0300

    add 2 hometask

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items';
 
 import SimpleLightbox from 'simplelightbox';
-import '../sass/simple-lightbox.scss';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 const refs = {
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
 
+const refs = {
+    iframe: document.querySelector('#vimeo-player'),
+};
+
+const player = new Vimeo(refs.iframe);
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedSavedTime = JSON.parse(savedTime);
+
+if (localStorage.getItem('videoplayer-current-time')) {
+    player.setCurrentTime(parsedSavedTime.seconds);
+}
+
+player.on('timeupdate', function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+});
